fd11ed5b0cec9bbfd78db4e21035a7730a5c1a48
ijaz bacha <ijazbacha14@gmail.com>
2022-11-24 10:10:30 +0500

    stack navigation

--- LearnMaui/App.xaml.cs
+++ LearnMaui/App.xaml.cs
@@ -5,7 +5,9 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
-
-		MainPage = new MainPage();
+		var NavPage = new NavigationPage(new MainPage());
+		NavPage.BarBackground = Colors.Black;
+        NavPage.BarTextColor = Colors.White;
+        MainPage = NavPage;
 	}
 }
--- LearnMaui/MainPage.xaml.cs
+++ LearnMaui/MainPage.xaml.cs
@@ -11,14 +11,15 @@ public partial class MainPage : ContentPage
 
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
-		count++;
+		Navigation.PushAsync(new DemoPage());
+		//count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+		//if (count == 1)
+		//	CounterBtn.Text = $"Clicked {count} time";
+		//else
+		//	CounterBtn.Text = $"Clicked {count} times";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		//SemanticScreenReader.Announce(CounterBtn.Text);
 	}
 }
 
