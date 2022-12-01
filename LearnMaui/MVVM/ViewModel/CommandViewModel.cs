86208c0778cdb2b6f685ec102a92667c0af50800
ijaz bacha <ijazbacha14@gmail.com>
2022-12-01 12:21:39 +0500

    Calculator with AddINotifyPropertyChangedInterface

--- LearnMaui/MVVM/ViewModel/CommandViewModel.cs
+++ LearnMaui/MVVM/ViewModel/CommandViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PropertyChanged;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,8 +8,23 @@ using System.Windows.Input;
 
 namespace LearnMaui.MVVM.ViewModel
 {
+    [AddINotifyPropertyChangedInterface]
     public class CommandViewModel
-    { 
+    {
+        public int First { get; set; }
+        public int Second { get; set; }
+        public int Result { get; set; }
+
+        public ICommand ResultCommand =>
+            new Command(Calculate);
+
+        private void Calculate(object obj)
+        {
+            Result = First + Second;
+        }
+
+
+
         public string searchCommand { get; set; }
         public ICommand commandSearch =>
             new Command(Commandpara);
