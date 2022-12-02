641525fe72da91a2347692755ee3ed9b606020d2
ijaz bacha <ijazbacha14@gmail.com>
2022-12-02 11:35:18 +0500

    Refresh View

--- LearnMaui/MVVM/ViewModel/ClothViewModel.cs
+++ LearnMaui/MVVM/ViewModel/ClothViewModel.cs
@@ -1,18 +1,37 @@
 ﻿using LearnMaui.MVVM.Models;
+using PropertyChanged;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace LearnMaui.MVVM.ViewModel
 {
+    [AddINotifyPropertyChangedInterface]
     public class ClothViewModel
     {
         public ObservableCollection<Cloth> Cloths { get; set; }
+        public bool IsRefresh { get; set; }
+
+        public ICommand RefreshCommand =>
+            new Command(async () =>
+            {
+                IsRefresh = true;
+                await Task.Delay(2000);
+                ItemRefresh();
+                IsRefresh = false;
+            });
+
 
         public ClothViewModel()
+        {
+            ItemRefresh();
+        }
+
+        private void ItemRefresh()
         {
             Cloths = new ObservableCollection<Cloth>()
             {
